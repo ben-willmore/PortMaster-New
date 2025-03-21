40008234bc215ab0272708bc9569284b913e2643
Ben Willmore <ben@willmore.eu>
2025-03-21 19:17:07 +0000

    F1spirit build

--- ports/f1spirit/F1 Spirit.sh
+++ ports/f1spirit/F1 Spirit.sh
@@ -13,7 +13,7 @@ else
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/device_info.txt
+
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
 get_controls
@@ -35,10 +35,10 @@ export LD_LIBRARY_PATH="$GAMEDIR/libs:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" 
 export TEXTINPUTINTERACTIVE="Y"
 
-$ESUDO chmod 666 /dev/uinput
 $GPTOKEYB "f1spirit" -c "f1spirit.gptk" &
- ./f1spirit
 
-$ESUDO kill -9 $(pidof gptokeyb)
-$ESUDO systemctl restart oga_events &
-printf "\033c" > /dev/tty0
+pm_platform_helper "$GAMEDIR/f1spirit"
+
+./f1spirit
+
+pm_finish
